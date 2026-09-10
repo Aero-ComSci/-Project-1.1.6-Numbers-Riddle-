183fa046bd77a237f39ef8beb89bd3877070c82b
anyatheonionslays <anyabhatnagar@gmail.com>
2026-09-10 11:17:32 -0700

    Add files via upload

---
+++ NumberRiddleOne.java
@@ -0,0 +1,20 @@
+//One test case
+public class NumberRiddleOne{
+    public static void main (String [] args){
+        int originalInput = 1; //Enter a test value
+        System.out.println("You chose: " + originalInput);
+        //Converting original number to a double data type
+        double currentInput = 2* originalInput;
+        System.out.println("Step 1 (Double the number): "+ currentInput);
+        // Adding 6 to the number
+        currentInput += 6;
+        System.out.println("Step 2 (Add 6 to number): "+ currentInput);
+        //Dividing the number by 2 
+        currentInput /=2;
+        System.out.println("Step 3 (Divide by 2): "+ currentInput);
+        // Subtracting the original number from the current number
+        currentInput = currentInput - originalInput;
+        System.out.println("Subtract Current by Original --> Final Output: "+ (int)currentInput); //Display the final output
+
+    }
+}
