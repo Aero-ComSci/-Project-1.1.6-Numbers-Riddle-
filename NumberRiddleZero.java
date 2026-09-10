dee62183d8f261181846ef6a3ed147c69dca6fac
anyatheonionslays <anyabhatnagar@gmail.com>
2026-09-10 11:25:10 -0700

    Update NumberRiddleZero.java

--- NumberRiddleZero.java
+++ NumberRiddleZero.java
@@ -4,16 +4,16 @@ public class NumberRiddleZero{
         int originalInput = 0; //Enter a test value
         System.out.println("You chose: " + originalInput);
         //Converting original number to a double data type
-        double currentInput = 2* originalInput;
+        double currentInput = 2* originalInput; // Multiply number by 2
         System.out.println("Step 1 (Double the number): "+ currentInput);
         // Adding 6 to the number
-        currentInput += 6;
+        currentInput += 6; 
         System.out.println("Step 2 (Add 6 to number): "+ currentInput);
         //Dividing the number by 2 
-        currentInput /=2;
+        currentInput /=2; 
         System.out.println("Step 3 (Divide by 2): "+ currentInput);
         // Subtracting the original number from the current number
-        currentInput = currentInput - originalInput;
+        currentInput = currentInput - originalInput; 
         System.out.println("Subtract Current by Original --> Final Output: "+ (int)currentInput); //Display the final output
 
     }
